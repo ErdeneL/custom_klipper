d88b3ea81e78ea8c20f86cfabb0f8228baa8ffa1
ErdeneL <erdene.lu.n@gmail.com>
2024-05-19 23:03:38 +0800

    custom

--- klippy/kinematics/custom.py
+++ klippy/kinematics/custom.py
@@ -7,9 +7,11 @@
 import math
 import stepper
 import logging
+import chelper
 
 class CustomKinematics:
     def __init__(self, toolhead, config):
+        
         self.printer = config.get_printer()
         self.rails = []
         for type in 'x':
@@ -26,13 +28,19 @@ class CustomKinematics:
     def calc_position(self, stepper_positions):
         pass
     def set_position(self, newpos, homing_axes):
-        pass
+        for i, rail in enumerate(self.rails):
+            rail.set_position(newpos)
     def home(self, homing_state):
+        self.home2(homing_state)
+
+    # using force_move, endstop_query
+    def home1(self, homing_state):
+        
         # force move stepper without kinematic
         force_move = self.printer.lookup_object('force_move')
         toolhead = homing_state.toolhead
+        toolhead.flush_step_generation()
         for rail in self.rails:
-            # TODO ignore bed stepper
             for stepper in rail.get_steppers():
                 move_end_print_time = toolhead.get_last_move_time()
                 done = 0
@@ -47,6 +55,23 @@ class CustomKinematics:
         curpos = toolhead.get_position()
         curpos[0] = 0
         toolhead.set_position(curpos, homing_axes=(0, 1, 2))
+
+    def home2(self, homing_state):
+        toolhead = self.printer.lookup_object('toolhead')
+        for axis, rail in enumerate(self.rails):
+            rail.setup_itersolve('cartesian_stepper_alloc', 'x'.encode())
+            position_min, position_max = rail.get_range()
+            hi = rail.get_homing_info()
+            homepos = [None, None, None, None]
+            homepos[axis] = hi.position_endstop
+            forcepos = list(homepos)
+            if hi.positive_dir:
+                forcepos[axis] -= 1.5 * (hi.position_endstop - position_min)
+            else:
+                forcepos[axis] += 1.5 * (position_max - hi.position_endstop)
+            homing_state.home_rails([rail], forcepos, homepos)
+            rail.setup_itersolve('cartesian_stepper_alloc', 'x'.encode())
+        toolhead.set_position([0,0,0,0], homing_axes=(0))
 
     def check_move(self, move):
         pass
